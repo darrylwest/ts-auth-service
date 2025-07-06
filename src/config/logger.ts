e9881e641e8742e85ee13c07cfcb6478af71822a
darryl west <darryl.west@raincitysoftware.com>
2025-07-06 13:00:25 -0700

    added rolling file logger

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,4 +1,5 @@
 import { createLogger, transports, format } from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
 
 const logger = createLogger({
   level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
@@ -18,8 +19,15 @@ const logger = createLogger({
 });
 
 if (process.env.NODE_ENV !== 'production') {
-  logger.add(new transports.File({ filename: 'logs/error.log', level: 'error' }));
-  logger.add(new transports.File({ filename: 'logs/combined.log' }));
+  const fileRotateTransport = new DailyRotateFile({
+    filename: 'logs/application-%DATE%.log',
+    datePattern: 'YYYY-MM-DD',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d',
+  });
+
+  logger.add(fileRotateTransport);
 }
 
 export default logger;
