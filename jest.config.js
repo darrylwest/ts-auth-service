f28e969d6eed812f524537332842bd1da7290134
darryl west <darryl.west@raincitysoftware.com>
2025-07-06 09:36:52 -0700

    added jest and supertest

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+// jest.config.js
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  // Automatically clear mock calls, instances, contexts and results before every test
+  clearMocks: true,
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+  // A list of paths to directories that Jest should use to search for files in
+  roots: ['<rootDir>/src'],
+  // The test match pattern
+  testMatch: ['**/__tests__/**/*.test.ts'],
+};
