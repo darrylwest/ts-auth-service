b00c3e8d7c148b2a1a28c85954176bcb13b910e0
darryl west <darryl.west@raincitysoftware.com>
2025-07-06 14:17:40 -0700

    commented out the redis setup for now

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,9 +5,10 @@ import { UserProfile } from '../types/models';
 let userStore: Keyv<UserProfile>;
 
 if (process.env.NODE_ENV === 'production' && process.env.REDIS_URL) {
-  console.log('Connecting to Redis...');
+  // console.log('Connecting to Redis...');
   // The type <UserProfile> tells Keyv that it will be storing UserProfile objects.
-  userStore = new Keyv<UserProfile>(process.env.REDIS_URL, { namespace: 'users' });
+  // userStore = new Keyv<UserProfile>(process.env.REDIS_URL, { namespace: 'users' });
+  userStore = new Keyv<UserProfile>({ namespace: 'users' });
 } else {
   console.log('Using in-memory store for development.');
   userStore = new Keyv<UserProfile>({ namespace: 'users' });
